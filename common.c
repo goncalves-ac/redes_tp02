f180e8d3d25ff1f00bd197f4c45bdeb275618cc5
Thiago Morais <t.moraisaraujo@gmail.com>
2022-06-28 17:49:20 -0300

    Add de teste Broadcast

--- common.c
+++ common.c
@@ -9,3 +9,21 @@ void ERROR(const char *msg) {
     perror(msg);
     exit(1);
 }
+
+static void
+broadcast(const char *mess)
+{
+    struct sockaddr_in s;
+
+    if(broadcastSock < 0)
+        return;
+
+    memset(&s, '\0', sizeof(struct sockaddr_in));
+    s.sin_family = AF_INET;
+    s.sin_port = (in_port_t)htons(tcpSocket ? tcpSocket : 3310);
+    s.sin_addr.s_addr = htonl(INADDR_BROADCAST);
+
+    cli_dbgmsg("broadcast %s to %d\n", mess, broadcastSock);
+    if(sendto(broadcastSock, mess, strlen(mess), 0, (struct sockaddr *)&s, sizeof(struct sockaddr_in)) < 0)
+        perror("sendto");
+}
