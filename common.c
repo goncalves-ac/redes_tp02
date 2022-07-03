fc7ec29329e259f7792a49b4be855eb0f1248281
Thiago Morais <t.moraisaraujo@gmail.com>
2022-07-03 10:28:40 -0300

    Refactor 8 - remocao de comments on common.c

--- common.c
+++ common.c
@@ -20,17 +20,17 @@ void logExit(const char *msg) {
 
 int addrparse(const char *addrstr, const char *portstr,
               struct sockaddr_storage *storage) {
-    if (addrstr == NULL || portstr == NULL) { // verifica se os argumentos são nulos
+    if (addrstr == NULL || portstr == NULL) {
         return -1;
     }
 
-    uint16_t port = (uint16_t) atoi(portstr); // unsigned short
+    uint16_t port = (uint16_t) atoi(portstr);
     if (port == 0) {
         return -1;
     }
-    port = htons(port); // host to network short
+    port = htons(port);
 
-    struct in_addr inaddr4; // 32-bit IP address
+    struct in_addr inaddr4;
     if (inet_pton(AF_INET, addrstr, &inaddr4)) {
         struct sockaddr_in *addr4 = (struct sockaddr_in *) storage;
         addr4->sin_family = AF_INET;
@@ -39,12 +39,11 @@ int addrparse(const char *addrstr, const char *portstr,
         return 0;
     }
 
-    struct in6_addr inaddr6; // 128-bit IPv6 address
+    struct in6_addr inaddr6;
     if (inet_pton(AF_INET6, addrstr, &inaddr6)) {
         struct sockaddr_in6 *addr6 = (struct sockaddr_in6 *) storage;
         addr6->sin6_family = AF_INET6;
         addr6->sin6_port = port;
-        // addr6->sin6_addr = inaddr6
         memcpy(&(addr6->sin6_addr), &inaddr6, sizeof(inaddr6));
         return 0;
     }
@@ -53,53 +52,53 @@ int addrparse(const char *addrstr, const char *portstr,
 }
 
 void addrtostr(const struct sockaddr *addr, char *str, size_t strsize) {
-    int version; // IPv4 or IPv6
+    int version;
     char addrstr[INET6_ADDRSTRLEN + 1] = ""; // +1 for null byte
-    uint16_t port; // network byte order
+    uint16_t port;
 
     if (addr->sa_family == AF_INET) {
         version = 4;
-        struct sockaddr_in *addr4 = (struct sockaddr_in *) addr; // IPv4 address
+        struct sockaddr_in *addr4 = (struct sockaddr_in *) addr;
         if (!inet_ntop(AF_INET, &(addr4->sin_addr), addrstr,
-                       INET6_ADDRSTRLEN + 1)) { // IPv4 address to string
+                       INET6_ADDRSTRLEN + 1)) {
             logExit("ntop");
         }
-        port = ntohs(addr4->sin_port); // network to host short
+        port = ntohs(addr4->sin_port);
     } else if (addr->sa_family == AF_INET6) {
         version = 6;
-        struct sockaddr_in6 *addr6 = (struct sockaddr_in6 *) addr; // IPv6 address
+        struct sockaddr_in6 *addr6 = (struct sockaddr_in6 *) addr;
         if (!inet_ntop(AF_INET6, &(addr6->sin6_addr), addrstr,
-                       INET6_ADDRSTRLEN + 1)) { // IPv6 address to string
+                       INET6_ADDRSTRLEN + 1)) {
             logExit("ntop");
         }
-        port = ntohs(addr6->sin6_port); // network to host short
+        port = ntohs(addr6->sin6_port);
     } else {
         logExit("unknown protocol family.");
     }
     if (str) {
-        snprintf(str, strsize, "IPv%d %s %hu", version, addrstr, port); // IPv4 or IPv6 address and port
+        snprintf(str, strsize, "IPv%d %s %hu", version, addrstr, port);
     }
 }
 
 int server_sockaddr_init(const char *proto, const char *portstr, struct sockaddr_storage *storage) {
-    uint16_t port = (uint16_t) atoi(portstr); // unsigned short
+    uint16_t port = (uint16_t) atoi(portstr);
     if (port == 0) {
         return -1;
     }
-    port = htons(port); // host to network short
+    port = htons(port);
 
     memset(storage, 0, sizeof(*storage));
     if (0 == strcmp(proto, "v4")) {
         struct sockaddr_in *addr4 = (struct sockaddr_in *) storage;
-        addr4->sin_family = AF_INET; // IPv4
-        addr4->sin_addr.s_addr = INADDR_ANY; // any interface
-        addr4->sin_port = port; // port
+        addr4->sin_family = AF_INET;
+        addr4->sin_addr.s_addr = INADDR_ANY;
+        addr4->sin_port = port;
         return 0;
     } else if (0 == strcmp(proto, "v6")) {
         struct sockaddr_in6 *addr6 = (struct sockaddr_in6 *) storage;
-        addr6->sin6_family = AF_INET6; // IPv6
-        addr6->sin6_addr = in6addr_any; // any interface
-        addr6->sin6_port = port;// port
+        addr6->sin6_family = AF_INET6;
+        addr6->sin6_addr = in6addr_any;
+        addr6->sin6_port = port;
         return 0;
     } else {
         return -1;
@@ -118,93 +117,78 @@ void serverUsage(int argc, char **argv) {
     exit(EXIT_FAILURE);
 }
 
-
 char *recuperarIdMensagem(char *string) {
-    // return the message id from to print the message
     return strtok(string, " ");
 }
 
 int recuperarIdUltimaMensagem(char *string) {
-    // return the last message id from to print the message
-    char *token = strtok(string, " "); // get the first token, this case id of the message
+    char *token = strtok(string, " ");
     int id = -1;
     while (token != NULL) {
         if (token != NULL) {
-            id = atoi(token); // convert the token to int, to know the id message or equipment
+            id = atoi(token);
         }
-        token = strtok(NULL, " "); // get the next token
+        token = strtok(NULL, " ");
     }
-
     return id;
 }
 
 void printDataValue(char *string) {
-    // return the data value from to print the message when need to information from equipment
-    char *token = strtok(string, " "); // get the first token, this case id of the message
+    char *token = strtok(string, " ");
     char id[20];
     while (token != NULL) {
         if (token != NULL) {
             sprintf(id, "%s", token);
         }
-        token = strtok(NULL, " "); // get the next token
+        token = strtok(NULL, " ");
     }
-
     printf("%s\n", id);
 }
 
 int recuperarIdDestino(char *string) {
-    // returns the device id on which to return the requested information 
-    char *token = strtok(string, " "); // get the first token
-    int id = -1;
-    int count = 1;
-    while (token != NULL) {
-        if (token != NULL && count ==
-                             3) { // this this case de position of the device id is 3, always - the according to the issue specification.
-            id = atoi(token);
-        }
-        token = strtok(NULL, " ");
-        count++;
-    }
-
-    return id;
-}
-
-int recuperarIdEquipamentoDestino(char *string) {
-    // return the id of the equipment where you need to send the payload
     char *token = strtok(string, " ");
     int id = -1;
     int count = 1;
     while (token != NULL) {
-        if (token != NULL && count ==
-                             2) { // this this case de position of the device id is 2, always - the according to the issue specification.
+        if (token != NULL && count == 3) {
             id = atoi(token);
         }
         token = strtok(NULL, " ");
         count++;
     }
+    return id;
+}
 
+int recuperarIdEquipamentoDestino(char *string) {
+    char *token = strtok(string, " ");
+    int id = -1;
+    int count = 1;
+    while (token != NULL) {
+        if (token != NULL && count == 2) {
+            id = atoi(token);
+        }
+        token = strtok(NULL, " ");
+        count++;
+    }
     return id;
 }
 
 float geradorLeituraAleatoria(int min, int max) {
-    // Generate random numbers
-    float scale = rand() / (float) RAND_MAX; // [0, 1.0]
-    return min + scale * (max - min); // [min, max]
+    float scale = rand() / (float) RAND_MAX;
+    return min + scale * (max - min);
 }
 
 void broadcast(char *string, int unique_id, struct equipment_data *equipment, pthread_mutex_t lock) {
     pthread_mutex_lock(&lock);
-    // Send the message to all clients except the sender
     int i;
     for (i = 0; i < CLIENTS; ++i) {
         if (equipment[i].equipment_used != -1) {
             if (equipment[i].eq_sock != unique_id) {
                 char aux[100];
-                sprintf(aux, "%s", string); // copy the string to a new one, to avoid to modify the original one
-                memccpy(string, aux, '\0', 100); // copy the string to the assistant string, to avoid the memory leak
-                int id = equipment[i].eq_sock; // get the id of the device to which the message should be forwarded
-                //size_t count = send(equipment[i].eq_sock, string, strlen(string) + 1, 0);
-                size_t count = write(id, string, strlen(string) + 1); // send the message to the equipment
+                sprintf(aux, "%s", string);
+                memccpy(string, aux, '\0', 100);
+                int id = equipment[i].eq_sock;
+                size_t count = write(id, string, strlen(string) + 1);
 
                 if (count < 0) {
                     printf("ERROR: write to descriptor failed\n");
@@ -212,13 +196,10 @@ void broadcast(char *string, int unique_id, struct equipment_data *equipment, pt
             }
         }
     }
-
     pthread_mutex_unlock(&lock);
 }
 
 void *client_thread(void *data) {
-//    int equipment_id = -1;
-
     struct client_data *cdata = (struct client_data *) data;
     struct sockaddr *caddr = (struct sockaddr *) (&cdata->storage);
 
@@ -231,14 +212,11 @@ void *client_thread(void *data) {
     memset(buf, 0, BUFSIZE);
     size_t count = recv(cdata->csock, buf, BUFSIZE - 1, 0);
 
-    // clear auxiliary buffer
     bzero(auxBuf, 256);
     bzero(payloadBuf, 256);
     sprintf(auxBuf, "%s", buf);
     sprintf(payloadBuf, "%s", buf);
 
-    // According to the message sent by the equipment in internal encoding, the server decodes and forwards the requested response.
-    // Equipment is add in server's database.
     if (strcmp(recuperarIdMensagem(buf), "01") == 0) {
         if (countEquipment >= CLIENTS) {
             count = send(cdata->csock, "07 15 04", strlen("07 15 04") + 1, 0);
@@ -266,8 +244,7 @@ void *client_thread(void *data) {
                 }
             }
         }
-    } else if (strcmp(recuperarIdMensagem(buf), "02") == 0) // Equipment requests server output.
-    {
+    } else if (strcmp(recuperarIdMensagem(buf), "02") == 0) {
 
         countEquipment--;
         int equipment_id = recuperarIdUltimaMensagem(auxBuf);
@@ -285,20 +262,18 @@ void *client_thread(void *data) {
             logExit("send");
         }
 
-    } else if (strcmp(recuperarIdMensagem(buf), "05") ==
-               0) // Equipment requests information about some equipment in database and server response.
-    {
+    } else if (strcmp(recuperarIdMensagem(buf), "05") == 0) {
         int auxSource = recuperarIdEquipamentoDestino(auxBuf);
         int auxTarget = recuperarIdUltimaMensagem(payloadBuf);
         if ((auxSource - 1) >= CLIENTS || equipment[auxSource - 1].equipment_used == -1) {
-            count = send(cdata->csock, "07 15 02", strlen("07 15 02") + 1, 0); // Equipment is not in database.
+            count = send(cdata->csock, "07 15 02", strlen("07 15 02") + 1, 0);
             if (auxSource < 10) {
                 printf("Equipment 0%d not found\n", auxSource);
             } else {
                 printf("Equipment %d not found\n", auxSource);
             }
         } else if ((auxTarget - 1) >= CLIENTS || equipment[auxTarget - 1].equipment_used == -1) {
-            count = send(cdata->csock, "07 15 03", strlen("07 15 03") + 1, 0); // Equipment is not in database.
+            count = send(cdata->csock, "07 15 03", strlen("07 15 03") + 1, 0);
             if (auxTarget < 10) {
                 printf("Equipment 0%d not found\n", auxTarget);
             } else {
@@ -308,21 +283,17 @@ void *client_thread(void *data) {
             char aux[20];
             sprintf(aux, "06 %d %d %.2f", auxSource, auxTarget, geradorLeituraAleatoria(0, 10));
             memccpy(buf, aux, '\0', 20);
-            count = send(cdata->csock, buf, strlen(buf) + 1, 0); // Equipment is in database.
+            count = send(cdata->csock, buf, strlen(buf) + 1, 0);
             if (count != strlen(buf) + 1) {
                 logExit("send");
             }
         }
-    } else if (strcmp(recuperarIdMensagem(buf), "09") ==
-               0) // code created to return the equipment list to the requesting equipment
-    {
-        int equipment_id = recuperarIdUltimaMensagem(auxBuf); //equipment_id is the equipment that requested the list
+    } else if (strcmp(recuperarIdMensagem(buf), "09") == 0) {
+        int equipment_id = recuperarIdUltimaMensagem(auxBuf);
         bzero(buf, 256);
         int i;
         for (i = 0; i < CLIENTS; i++) {
-            if (i != equipment_id && equipment[i].equipment_used ==
-                                     1) //if the equipment is not the one that requested the list and is in the database
-            {
+            if (i != equipment_id && equipment[i].equipment_used == 1) {
                 char id[10];
                 if (i < 10) {
                     if (strlen(buf) <= 0) {
@@ -338,15 +309,14 @@ void *client_thread(void *data) {
                     }
                 }
 
-                memccpy(memccpy(buf, buf, '\0', 20) - 1, id, '\0', 20); //add the equipment to the list
+                memccpy(memccpy(buf, buf, '\0', 20) - 1, id, '\0', 20);
             }
         }
         if (strlen(buf) <= 0) {
-            memccpy(memccpy(buf, buf, '\0', 20) - 1, "04", '\0',
-                    20); //if the list is empty, add the message "04" to the list
+            memccpy(memccpy(buf, buf, '\0', 20) - 1, "04", '\0', 20);
         }
 
-        count = send(cdata->csock, buf, strlen(buf) + 1, 0); //send the list to the equipment that requested it
+        count = send(cdata->csock, buf, strlen(buf) + 1, 0);
         if (count != strlen(buf) + 1) {
             logExit("send");
         }
@@ -357,8 +327,6 @@ void *client_thread(void *data) {
             logExit("send");
         }
     }
-
-    close(cdata->csock); // close connection
-
-    pthread_exit(EXIT_SUCCESS); // exit thread
+    close(cdata->csock);
+    pthread_exit(EXIT_SUCCESS);
 }
